7a675b3e20f8dc302b49c946b3b0a082f0161120
d6byoung <49166327+d6byoung@users.noreply.github.com>
2019-04-01 14:03:57 -0400

    hello NMC

---
+++ Github trial/Github trial/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Github_trial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello NMC! We'll be friends forever!");
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
